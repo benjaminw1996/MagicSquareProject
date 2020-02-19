b45c6f7102deb865cc903058193d1db25e3d8c08
Ben Warren <33360056+benw1996@users.noreply.github.com>
2020-02-19 18:30:45 +0000

    Added in working functions to fill a random integer array.
    Instead of filling the integer arrays with random numbers, I am now filling them in order and then using a different function to then jumble up the array.
    Added in a small function that will print the array to the console, this was done to save having to write in Writeline and Write commands when I want to see the results.

--- MagicSquareProject/Program.cs
+++ MagicSquareProject/Program.cs
@@ -16,6 +16,11 @@ namespace MagicSquareProject{
         static void Main(string[] args){
             Console.WriteLine("Hello, here's to a new beginning...");
 
+            //User is asked to input the desired width of the magic square
+            Console.Write("Please input width of desired magic square - ");
+            m_size = Int16.Parse(Console.ReadLine());
+            m_numberOfSquares = m_size * m_size;
+
             //Magic square generator function is called 
             MagicSquareGenerator();
 
@@ -28,24 +33,68 @@ namespace MagicSquareProject{
         /// Function to generate a new random integer array to then be tested to see if it is a magic square
         /// </summary>
         public static void MagicSquareGenerator() {
-            //User is asked to input the desired width of the magic square
-            Console.Write("Please input width of desired magic square - ");
-            m_size = Int16.Parse(Console.ReadLine());
+            //FillIntegerArray function is called to fill the array, the DisplayArray function is then called to display the output
+            FillIntegerArray();
+            DisplayArray();
 
-            //The integer array to be filled is declared with the given size
+            //RandomiseArray function is called to randomise the array that was just filled, the DisplayArray function is then called to display the output
+            RandomiseArray();
+            DisplayArray();
+        }
+
+        /// <summary>
+        /// Function to fill the integer array in order from smallest to largest
+        /// </summary>
+        public static void FillIntegerArray() {
+            //Initialises the integer array with the size given by the user
             m_magicSquare = new int[m_size, m_size];
+            //Initilises a new integer that will be incremented and used to copy into the integer array.
+            int m_number = 1;
 
-            //The number of squares inside the array is calculated from the width of the array
-            m_numberOfSquares = m_size * m_size;
-            
-            //Integer array is looped through and filled with random integers
-            for(int i = 0; i < m_size; i++) {
-                Console.WriteLine("");
-                for(int j = 0; j < m_size; j++) {
-                    m_magicSquare[i, j] = random.Next(0, m_numberOfSquares + 1);
-                    Console.Write(m_numberOfSquares + " ");
+            //Looping through the array and inputing the numbers from the integer above.
+            for (int i = 0; i < m_size; i++) {
+                for (int j = 0; j < m_size; j++) {
+                    m_magicSquare[i, j] = m_number;
+                    m_number++;
                 }
             }
+        }
+
+        /// <summary>
+        /// Function to randomise the integer array by swapping around values based on a random index.
+        /// </summary>
+        public static void RandomiseArray() {
+            //Declaring local variables to be used within this function. A temp int to hold the value being swapped, and two integers to hold the random index
+            int temp = 0;
+            int x_index = 0;
+            int y_index = 0;
+
+            //Looping through the array and swapping the numbers in sequence with a random index within the array. 
+            for (int i = 0; i < m_size; i++) {
+                for (int j = 0; j < m_size; j++) {
+                    temp = m_magicSquare[i, j];
+
+                    x_index = random.Next(0, m_size);
+                    y_index = random.Next(0, m_size);
+
+                    m_magicSquare[i, j] = m_magicSquare[x_index, y_index];
+                    m_magicSquare[x_index, y_index] = temp;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Simple function to write out the array in the console app.
+        /// This was written to save having to add Console.WriteLine and Console.Write all over the place whenever I wanted to display the array.
+        /// </summary>
+        public static void DisplayArray() {
+            for (int i = 0; i < m_size; i++) {
+                Console.WriteLine();
+                for (int j = 0; j < m_size; j++) {
+                    Console.Write(m_magicSquare[i, j] + " ");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
